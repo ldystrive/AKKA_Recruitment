08aac12ebd25aec73200e39d648de90899fc588b
ldystrive <594770590@qq.com>
2019-05-28 22:56:09 +0800

    delete State&Data

--- src/main/java/cn/fdu/akka/recruitment/common/Resume.java
+++ src/main/java/cn/fdu/akka/recruitment/common/Resume.java
@@ -1,17 +1,20 @@
 package cn.fdu.akka.recruitment.common;
 
 import akka.actor.ActorRef;
+import cn.fdu.akka.recruitment.FSM.Applicant;
 
 public final class Resume {
 
 	private String name;
 	private String position;
-	private final ActorRef hr;
+	private final ActorRef hrRef;
+	private final ActorRef applicantRef;
 
-	public Resume(String name, String position, ActorRef hr) {
+	public Resume(String name, String position, ActorRef hrRef, ActorRef applicantRef) {
 		this.name = name;
 		this.position = position;
-		this.hr = hr;
+		this.hrRef = hrRef;
+		this.applicantRef = applicantRef;
 	}
 
 	public String toString() {
