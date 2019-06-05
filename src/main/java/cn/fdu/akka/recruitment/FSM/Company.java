98cfef088ef9fc1a2f4a0549a9ee0d83c8a80266
ldystrive <594770590@qq.com>
2019-06-05 12:43:57 +0800

    Company V1

--- src/main/java/cn/fdu/akka/recruitment/FSM/Company.java
+++ src/main/java/cn/fdu/akka/recruitment/FSM/Company.java
@@ -15,13 +15,26 @@ import static cn.fdu.akka.recruitment.FSM.Company.Data;
 public class Company extends AbstractFSM<State, Data>{
 
 	{
-		startWith(Ready, new Data());
+		startWith(Init, new Data());
+
+		when(
+			Init,
+			matchEvent(
+				ActorRef.class,
+				Data.class,
+				(actorRef, data) -> {
+					return goTo(Ready).using(data.addActorRef(actorRef));
+				}
+			)
+		);
+
 		when(Ready,
 			matchEvent(
 				Position.class,
 				Data.class,
 				(position, data) -> {
 					final ActorRef positionOfCom = getContext().actorOf(Props.create(ComPos.class));
+					positionOfCom.tell(data.getActorRef(), getSelf());
 					System.out.println("upload a new position");
 					positionOfCom.tell(position, getSelf());
 					return stay();
@@ -31,9 +44,23 @@ public class Company extends AbstractFSM<State, Data>{
 		initialize();
 	}
 
-	enum State{Ready}
+	enum State{Init, Ready}
+
 
 	static final class Data{
+		private final ActorRef HR;
+		Data() {HR = null;}
+		Data(ActorRef hr) {
+			this.HR = hr;
+		}
+		Data(Data d) {
+			this.HR = d.HR;
+		}
+		Data addActorRef(ActorRef a) {
+			Data d = new Data(a);
+			return d;
+		}
+		ActorRef getActorRef() {return HR;}
 
 	}
 }
